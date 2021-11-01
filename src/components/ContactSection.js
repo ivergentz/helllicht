cd6e0ed694e671f55b2f41f091258f40c5a917fe
iver gentz <ivergentz@gmail.com>
2021-11-01 20:59:44 +0100

    Added image in Contact Section

--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -1,7 +1,38 @@
 import React from 'react'
+import styled from 'styled-components'
+import portrait from '../images/iver.png'
 
 const ContactSection = () => {
-  return <div>moinsen</div>
+  return (
+    <ContactContainer>
+      <Img src={portrait} alt="foto iver gentz" />
+      <p>
+        <strong>Iver Gentz</strong>
+        <br />
+        Der neue Typ
+      </p>
+    </ContactContainer>
+  )
 }
 
 export default ContactSection
+
+const ContactContainer = styled.section`
+  display: block;
+  width: 100%;
+  text-align: center;
+  p {
+    margin: 1rem auto;
+    font-size: 1rem;
+    line-height: 1.2rem;
+  }
+`
+
+const Img = styled.img`
+  margin: 0 auto;
+  height: 150px;
+  width: 150px;
+  object-fit: contain;
+  border-radius: 200px;
+  border: 1px solid var(--grey);
+`
